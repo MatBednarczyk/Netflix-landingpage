721daa94726b8eca1b36551ec86695b3dfdb4053
Mateusz Bednarczyk <mateuszbednarczyk.95@wp.pl>
2021-01-12 21:29:11 +0100

    JS added, DOM fixes

---
+++ main.js
@@ -0,0 +1,15 @@
+//Default Hide view
+let answear1 = document.getElementById('answear1').style.display = 'none';
+let answear2 = document.getElementById('answear2').style.display = 'none';
+let answear3 = document.getElementById('answear3').style.display = 'none';
+let answear4 = document.getElementById('answear4').style.display = 'none';
+let answear5 = document.getElementById('answear5').style.display = 'none';
+
+//Show/Hide Answear
+function showAnswear(id) {
+  let answear = document.getElementById(id);
+  if(answear.style.display == 'block')
+      answear.style.display = 'none';
+  else
+      answear.style.display = 'block';
+}
